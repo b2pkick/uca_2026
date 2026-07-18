fa5a1d9c37ef4f7f0e7d345e9afaf1082944aaa7
b2pkick <rahulgupta79777@gmail.com>
2026-07-18 11:06:46 +0530

    reverse

--- cPrograms/reverse.c
+++ cPrograms/reverse.c
@@ -4,7 +4,7 @@
 void reverse(char* p) {
     int i = 0;
     int j = strlen(p)-1;
-    while(i<=j) {
+    while(i < j) {
         char temp = p[i];
         p[i] = p[j];
         p[j] = temp;
@@ -16,7 +16,9 @@ void reverse(char* p) {
 int main() {
     char str[100];
 
-    scanf("%s", str);
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
 
     reverse(str);
 
